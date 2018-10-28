e2acd4f2b0b2e2a9138570a5d9c62b394eeaa948
Pedram <tadayoni@gmail.com>
2018-10-28 03:10:53 -0700

    Added new detail edit activity, removed venue activity, added google places

---
+++ app/src/main/java/net/tirgan/mex/ui/detail/DetailEditActivity.java
@@ -0,0 +1,13 @@
+package net.tirgan.mex.ui.detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_edit);
+    }
+}
---
+++ app/src/main/java/net/tirgan/mex/ui/detail/MexEditDialogFragment.java
@@ -0,0 +1,6 @@
+package net.tirgan.mex.ui.detail;
+
+import android.app.DialogFragment;
+
+public class MexEditDialogFragment extends DialogFragment {
+}
